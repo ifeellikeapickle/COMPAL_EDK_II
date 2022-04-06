c475ffd0f50990ba9c776263daf4446b4fcbc826
Chiu. Benson (TPE) <Benson_Chiu@compal.com>
2022-04-06 17:14:38 +0800

    20220406_1714

--- MyPkg/Application/ACPI/ACPI.c
+++ MyPkg/Application/ACPI/ACPI.c
@@ -20,19 +20,42 @@ UefiMain(
     IN EFI_SYSTEM_TABLE  *SystemTable
 )
 {
-    EFI_STATUS                  Status;
-    EFI_CONFIGURATION_TABLE		*AcpiTable = NULL;
-    //UINTN                       Index;
+    EFI_STATUS                                      Status;
+    UINT16                                          RsdpSignature[20];
+    UINT16                                          XsdtSignature[20];
+    EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER    *RSDP;
+    EFI_ACPI_DESCRIPTION_HEADER                     *XSDT;
+    //EFI_ACPI_DESCRIPTION_HEADER                     *DSDT;
+    UINTN                                           Index;
 
-    Status = EfiGetSystemConfigurationTable (&gEfiAcpi20TableGuid, (VOID **)&AcpiTable);
+    Status = EfiGetSystemConfigurationTable (&gEfiAcpi20TableGuid, (VOID **)&RSDP);
 
-    if (AcpiTable == NULL) {
-        Print (L"Get ACPI Table error.\n");
+    if (RSDP == NULL) {
+        Print (L"Get ACPI RSDP error.\n");
         Status = EFI_NOT_FOUND;
     } else {
-        Print (L"Found ACPI table: \n");
-		Print (L"[0x%x]\n", AcpiTable);
-        Status = PrintGuid (&(AcpiTable->VendorGuid));
+        Print (L"Found ACPI RSDP: \n");
+
+        Print (L"ACPI RSDP Address: [0x%x]\n", RSDP);
+
+        for (Index = 0; Index < 8; Index++) {
+            RsdpSignature[Index] = (UINT16)(RSDP->Signature >> (Index * 8) & 0xFF);
+        }
+
+        Print (L"   RSDP Signature: %s\n", RsdpSignature);
+        Print (L"         Revision: %d\n", RSDP->Revision);
+        Print (L"     RSDT Address: [0x%x], Length: %d\n", RSDP->RsdtAddress, RSDP->Length);
+		Print (L"     XSDT Address: [0x%lx]\n", RSDP->XsdtAddress);
+
+        if (RSDP->Revision >= EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER_REVISION) {
+
+            XSDT = (EFI_ACPI_DESCRIPTION_HEADER *)(RSDP->XsdtAddress);
+
+            for (Index = 0; Index < 4; Index++) {
+                XsdtSignature[Index] = (UINT16)(XSDT->Signature >> (Index * 8) & 0xFF);
+            }
+            Print (L"   XSDT Signature: %s\n", XsdtSignature);
+        }
     }
 
     return Status;
