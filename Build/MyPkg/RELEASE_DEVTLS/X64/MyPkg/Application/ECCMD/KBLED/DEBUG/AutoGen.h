4365b8399949fef6a05fde1a3379584cfdaf33b0
Chiu. Benson (TPE) <Benson_Chiu@compal.com>
2022-04-07 17:37:26 +0800

    20220407_1737

---
+++ Build/MyPkg/RELEASE_DEVTLS/X64/MyPkg/Application/ECCMD/KBLED/DEBUG/AutoGen.h
@@ -0,0 +1,74 @@
+/**
+  DO NOT EDIT
+  FILE auto-generated
+  Module name:
+    AutoGen.h
+  Abstract:       Auto-generated AutoGen.h for building module or library.
+**/
+
+#ifndef _AUTOGENH_a2ffee17_1ae4_4f97_b8c4_55f27cc2ead6
+#define _AUTOGENH_a2ffee17_1ae4_4f97_b8c4_55f27cc2ead6
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Base.h>
+#include <Uefi.h>
+
+extern GUID  gEfiCallerIdGuid;
+extern CHAR8 *gEfiCallerBaseName;
+
+#define EFI_CALLER_ID_GUID \
+  {0xa2ffee17, 0x1ae4, 0x4f97, {0xb8, 0xc4, 0x55, 0xf2, 0x7c, 0xc2, 0xea, 0xd6}}
+
+// Guids
+extern EFI_GUID gEfiMdePkgTokenSpaceGuid;
+extern EFI_GUID gEfiVTUTF8Guid;
+extern EFI_GUID gEfiVT100Guid;
+extern EFI_GUID gEfiVT100PlusGuid;
+extern EFI_GUID gEfiPcAnsiGuid;
+extern EFI_GUID gEfiUartDevicePathGuid;
+extern EFI_GUID gEfiSasDevicePathGuid;
+extern EFI_GUID gEfiVirtualDiskGuid;
+extern EFI_GUID gEfiVirtualCdGuid;
+extern EFI_GUID gEfiPersistentVirtualDiskGuid;
+extern EFI_GUID gEfiPersistentVirtualCdGuid;
+extern EFI_GUID gEfiEventReadyToBootGuid;
+extern EFI_GUID gEfiEventLegacyBootGuid;
+extern EFI_GUID gEfiGlobalVariableGuid;
+
+// Protocols
+extern EFI_GUID gEfiDevicePathProtocolGuid;
+extern EFI_GUID gEfiDebugPortProtocolGuid;
+extern EFI_GUID gEfiDriverBindingProtocolGuid;
+extern EFI_GUID gEfiSimpleTextOutProtocolGuid;
+extern EFI_GUID gEfiGraphicsOutputProtocolGuid;
+extern EFI_GUID gEfiHiiFontProtocolGuid;
+extern EFI_GUID gEfiUgaDrawProtocolGuid;
+extern EFI_GUID gEfiComponentNameProtocolGuid;
+extern EFI_GUID gEfiComponentName2ProtocolGuid;
+extern EFI_GUID gEfiDriverConfigurationProtocolGuid;
+extern EFI_GUID gEfiDriverConfiguration2ProtocolGuid;
+extern EFI_GUID gEfiDriverDiagnosticsProtocolGuid;
+extern EFI_GUID gEfiDriverDiagnostics2ProtocolGuid;
+
+// Definition of PCDs used in libraries is in AutoGen.c
+
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  );
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
