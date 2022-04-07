af3b6223bacce5144f9012e2abcb1a8a3db110a0
Chiu. Benson (TPE) <Benson_Chiu@compal.com>
2022-04-07 11:50:48 +0800

    20220407_1150

--- MyPkg/Application/ACPI/ACPI.c
+++ MyPkg/Application/ACPI/ACPI.c
@@ -6,10 +6,11 @@
 #include <Library/ShellCEntryLib.h>
 #include <Library/BaseMemoryLib.h>
 
-#include <Library/UefiBootServicesTableLib.h> //gST, gBS
+#include <Library/UefiBootServicesTableLib.h> // gST, gBS
 
+// ACPI
 #include <Guid/Acpi.h>
-#include <IndustryStandard/Acpi60.h>
+#include <IndustryStandard/Acpi.h>
 
 //#include <Protocol/SimpleFileSystem.h>
 #include "ACPI.h"
@@ -22,39 +23,62 @@ UefiMain(
 {
     EFI_STATUS                                      Status;
     UINT16                                          RsdpSignature[20];
+    UINT16                                          RsdpOemId[20];
     UINT16                                          XsdtSignature[20];
-    EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER    *RSDP;
-    EFI_ACPI_DESCRIPTION_HEADER                     *XSDT;
-    //EFI_ACPI_DESCRIPTION_HEADER                     *DSDT;
+    UINT64                                          *EntryPtr;
+    EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER    *Rsdp;
+    EFI_ACPI_DESCRIPTION_HEADER                     *Xsdt;
+    //EFI_ACPI_6_0_FIXED_ACPI_DESCRIPTION_TABLE       *Fadt;
+    //EFI_ACPI_DESCRIPTION_HEADER                     *Dsdt;
     UINTN                                           Index;
+    UINTN                                           EntryCount;
 
-    Status = EfiGetSystemConfigurationTable (&gEfiAcpi20TableGuid, (VOID **)&RSDP);
+    Status = EfiGetSystemConfigurationTable (&gEfiAcpi20TableGuid, (VOID **)&Rsdp);
 
-    if (RSDP == NULL) {
+    if (Rsdp == NULL) {
         Print (L"Get ACPI RSDP error.\n");
         Status = EFI_NOT_FOUND;
     } else {
         Print (L"Found ACPI RSDP: \n");
+        Print (L"     RSDP Address: [0x%x]\n", Rsdp);
 
-        Print (L"ACPI RSDP Address: [0x%x]\n", RSDP);
-
+        ZeroMem(RsdpSignature, sizeof(RsdpSignature));
         for (Index = 0; Index < 8; Index++) {
-            RsdpSignature[Index] = (RSDP->Signature >> (Index * 8) & 0xFF);
+            RsdpSignature[Index] = (Rsdp->Signature >> (Index * 8) & 0xFF);
+        }
+        ZeroMem(RsdpOemId, sizeof(RsdpOemId));
+        for (Index = 0; Index < 6; Index++) {
+            RsdpOemId[Index] = (Rsdp->OemId[Index] & 0xFF);
         }
 
         Print (L"   RSDP Signature: %s\n", RsdpSignature);
-        Print (L"         Revision: %d\n", RSDP->Revision);
-        Print (L"     RSDT Address: [0x%x], Length: %d\n", RSDP->RsdtAddress, RSDP->Length);
-		Print (L"     XSDT Address: [0x%lx]\n", RSDP->XsdtAddress);
+        Print (L"      RSDP OEM ID: %s\n", RsdpOemId);
+        Print (L"    RSDP Revision: %d\n", Rsdp->Revision);
+        Print (L"     RSDT Address: [0x%x], Length: %d\n", Rsdp->RsdtAddress, Rsdp->Length);
+		Print (L"     XSDT Address: [0x%lx]\n", Rsdp->XsdtAddress);
 
-        if (RSDP->Revision >= EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER_REVISION) {
+        if (Rsdp->Revision >= EFI_ACPI_6_0_ROOT_SYSTEM_DESCRIPTION_POINTER_REVISION) {
 
-            XSDT = (EFI_ACPI_DESCRIPTION_HEADER *)(RSDP->XsdtAddress);
+            Xsdt = (EFI_ACPI_DESCRIPTION_HEADER *)(Rsdp->XsdtAddress);
 
+            ZeroMem(XsdtSignature, sizeof(XsdtSignature));
             for (Index = 0; Index < 4; Index++) {
-                XsdtSignature[Index] = (XSDT->Signature >> (Index * 8) & 0xFF);
+                XsdtSignature[Index] = (Xsdt->Signature >> (Index * 8) & 0xFF);
             }
             Print (L"   XSDT Signature: %s\n", XsdtSignature);
+            Print (L"      XSDT Length: %d\n", Xsdt->Length);
+
+            EntryCount = (Xsdt->Length - sizeof (EFI_ACPI_DESCRIPTION_HEADER)) / sizeof (UINT64);
+            Print (L"   size of Header: %d\n", sizeof (EFI_ACPI_DESCRIPTION_HEADER));
+            Print (L"   size of UINT64: %d\n", sizeof (UINT64));
+            Print (L" XSDT Entry Count: %d\n", EntryCount);
+
+            EntryPtr = (UINT64 *)(Xsdt + 1);
+            /*
+            for (Index = 0; Index < EntryCount; Index++, EntryPtr++) {
+                
+            }
+            */
         }
     }
 
